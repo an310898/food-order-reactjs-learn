ac672e787129e0542fc521548136a26f1e466faf
anns318 <an310898@gmai.com>
2023-07-10 03:34:55 +0700

    understand clean up in useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,37 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = props => {
   const ctx = useContext(CartContext);
+  const [animationBtn, setAnimationBtn] = useState(false);
 
   const amountBadge = ctx.item.reduce((curNum, item) => {
     return curNum + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${animationBtn ? classes.bump : ""}`;
+  useEffect(() => {
+    console.log("effect run");
+    if (ctx.item.length === 0) {
+      return;
+    }
+    setAnimationBtn(true);
+
+    const timer = setTimeout(() => {
+      setAnimationBtn(false);
+    }, 300);
+
+    return () => {
+      console.log("clean up");
+      clearTimeout(timer);
+    };
+  }, [ctx.item]);
+
   return (
-    <button className={classes.button} onClick={props.onShowCart}>
+    <button className={btnClasses} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
